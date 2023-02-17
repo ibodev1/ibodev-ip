e6b7676509c8189271af5c3cdc03e3cb372b6115
ibodev1 <ibrahimodev42@gmail.com>
2023-02-17 15:28:07 +0300

    update: information /api route

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,19 @@
+import { readFileSync } from "fs";
+import path from "path";
 import { NextFunction, Request, Response } from "express";
 import app from "../app";
 
-app.get("/api/", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({
-    ip: "/api/ip",
-    useragent: "/api/useragent"
-  });
+app.get("/api/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const packagePath = path.join(process.cwd(), "package.json");
+    const packageData = JSON.parse(await readFileSync(packagePath, "utf8"));
+    res.status(200).json({
+      version: packageData.version,
+      ...packageData.response
+    });
+  } catch (error: any) {
+    res.status(400).end(error.toString());
+  }
 });
 
 export default app;
